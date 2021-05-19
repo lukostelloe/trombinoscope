fee447d76be1bf95455e5d0c9990e6600e69ac7b
lukostelloe <lukostelloe@gmail.com>
2021-05-19 14:29:06 +0200

    update

---
+++ PICCHANGE.js
@@ -0,0 +1,24 @@
+// function imgEnlarge(e) {
+//   if (e.style.width == "400px" || e.style.width == "") {
+//     e.style.width = "600px";
+//   } else {
+//     e.style.width = "400px";
+//   }
+
+//   var imgLength = document.getElementsByTagName("img").length;
+
+//   for (var i = 0; i < imgLength; i++) {
+//     if (document.getElementsByTagName("img")[i].id == e.id) {
+//       continue;
+//     }
+//     document.getElementsByTagName("img")[i].style.width = "400px";
+//   }
+// }
+
+function myFunction() {
+  if ((document.getElementById("castle").style.opacity = "100%")) {
+    document.getElementById("castle").style.opacity = "50%";
+  } else {
+    document.getElementById("castle").style.opacity = "100%";
+  }
+}
