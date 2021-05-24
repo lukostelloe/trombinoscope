5cd7b98c9eadc74918470251eac8e6e80ba28cf7
lukostelloe <lukostelloe@gmail.com>
2021-05-24 10:43:33 +0200

    monday morning

--- trombi.js
+++ trombi.js
@@ -1,20 +1,3 @@
-// function imgEnlarge(e) {
-//   if (e.style.width == "400px" || e.style.width == "") {
-//     e.style.width = "600px";
-//   } else {
-//     e.style.width = "400px";
-//   }
-
-//   var imgLength = document.getElementsByTagName("img").length;
-
-//   for (var i = 0; i < imgLength; i++) {
-//     if (document.getElementsByTagName("img")[i].id == e.id) {
-//       continue;
-//     }
-//     document.getElementsByTagName("img")[i].style.width = "400px";
-//   }
-// }
-
 function myFunction() {
   if ((document.getElementById("adrien").style.opacity = "100%")) {
     document.getElementById("adrien").style.opacity = "50%";
